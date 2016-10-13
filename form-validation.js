02c195829480dc9eee27d1a0202ad0a009834868
Jamie Van Winckel <jvanwinckel@mail.csuchico.edu>
2016-10-12 17:50:18 -0700

    made it to follow the advanced validation rules

--- form-validation.js
+++ form-validation.js
@@ -9,22 +9,54 @@ function validate(){
   }
   else
   {
-    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").innerHTML="Username must have at least 6 characters.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  if ( userEntered.search (" ") == -1 )
+  {
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else
+  {
+    document.getElementById("usernameError").innerHTML="Username cannot have any spaces.";
     document.getElementById("usernameError").classList.remove("hidden-message");
     document.getElementById("usernameError").classList.add("shown-message");
     document.getElementById("usernameGroup").classList.add("has-error");
   }
 
-  if ( passEntered != "password" )
+  if ( passEntered.toLowerCase(passEntered) != "password" )
   {
     document.getElementById("passwordGroup").classList.add("has-success");
   }
   else
   {
-    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").innerHTML="Password cannot be password.";
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     document.getElementById("passwordGroup").classList.add("has-error");
   }
-
+  if ( passEntered != userEntered )
+  {
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
+  else
+  {
+    document.getElementById("passwordError").innerHTML="Password cannot be the same as username.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  if ( passEntered.length >= 6 || passEntered <= 20 )
+  {
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
+  else
+  {
+    document.getElementById("passwordError").innerHTML="Password cannot be less than 6 nor greater than 20.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
 }
